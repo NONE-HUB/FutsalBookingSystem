3ad28cf37aff544e9880003e3d7a628b6f3cdf13
NO-ONE <mergichadmergi@gmail.com>
2025-05-25 20:02:51 +0545

    small

--- src/futsalbookingsystem/view/RegistrationView.java
+++ src/futsalbookingsystem/view/RegistrationView.java
@@ -9,6 +9,7 @@ public class RegistrationView extends javax.swing.JFrame {
         initComponents();
     }
 
+    
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
